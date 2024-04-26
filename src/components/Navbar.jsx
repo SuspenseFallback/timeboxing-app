01ca8090054232ecdc3ddc7e35433417a2a28c5b
Krithin Jay Pakshootra <krithinjaycode@gmail.com>
2024-04-27 07:48:53 +0800

    scaffold navbar

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,18 @@
+import {
+  NavigationMenu,
+  NavigationMenuContent,
+  NavigationMenuIndicator,
+  NavigationMenuItem,
+  NavigationMenuLink,
+  NavigationMenuList,
+  NavigationMenuTrigger,
+  NavigationMenuViewport,
+} from "@/components/ui/navigation-menu";
+
+import React, { useState, useEffect } from "react";
+
+const Navbar = () => {
+  return <></>;
+};
+
+export default Navbar;
