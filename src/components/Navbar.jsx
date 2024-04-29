baaf5183ce049454b0d7f52e9bb7c013deca36cc
Krithin Jay Pakshootra <krithinjaycode@gmail.com>
2024-04-29 18:56:27 +0800

    add account dropdown

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,11 +1,38 @@
 import React, { useState, useEffect } from "react";
 import { Outlet, useNavigate } from "react-router-dom";
 import "./Navbar.css";
+import { getUser } from "../firebase/firebase";
+import Spinner from "./Spinner";
+
+import { User, Settings, LogOut } from "lucide-react";
+
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
 
 const Navbar = () => {
   const navigate = useNavigate();
 
-  return (
+  const [loading, set_loading] = useState(true);
+  const [user, set_user] = useState(null);
+
+  useEffect(() => {
+    getUser((data) => {
+      set_user(data);
+      set_loading(false);
+    });
+  }, []);
+
+  return loading ? (
+    <div className="page">
+      <Spinner />
+    </div>
+  ) : (
     <>
       <div className="nav">
         <div className="nav-logo" onClick={() => navigate("/")}>
@@ -39,22 +66,49 @@ const Navbar = () => {
           >
             New timebox
           </div>
-          <div
-            className="nav-button"
-            onClick={() => {
-              navigate("/sign-up");
-            }}
-          >
-            Sign up
-          </div>
-          <div
-            className="nav-button"
-            onClick={() => {
-              navigate("/log-in");
-            }}
-          >
-            Log in
-          </div>
+          {user ? (
+            <>
+              <DropdownMenu>
+                <DropdownMenuTrigger>
+                  <div className="nav-button">My account</div>
+                </DropdownMenuTrigger>
+                <DropdownMenuContent>
+                  <DropdownMenuItem>
+                    <User className="mr-2 h-4 w-4" />
+                    <p className="text">Profile</p>
+                  </DropdownMenuItem>
+                  <DropdownMenuItem>
+                    <Settings className="mr-2 h-4 w-4" />
+                    <p className="text">Settings</p>
+                  </DropdownMenuItem>
+                  <DropdownMenuSeparator />
+                  <DropdownMenuItem>
+                    <LogOut className="mr-2 h-4 w-4" />
+                    <p className="text">Log out</p>
+                  </DropdownMenuItem>
+                </DropdownMenuContent>
+              </DropdownMenu>
+            </>
+          ) : (
+            <>
+              <div
+                className="nav-button"
+                onClick={() => {
+                  navigate("/sign-up");
+                }}
+              >
+                Sign up
+              </div>
+              <div
+                className="nav-button"
+                onClick={() => {
+                  navigate("/log-in");
+                }}
+              >
+                Log in
+              </div>
+            </>
+          )}
         </div>
       </div>
       <Outlet />
